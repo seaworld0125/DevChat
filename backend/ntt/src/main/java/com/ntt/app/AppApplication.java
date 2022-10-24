d6ff165d39ae6c0d988c40660e1085381f61177f
Tae kyoung Kim <mabrother98@gmail.com>
2022-10-24 17:58:51 +0900

    [feat] initial setting

---
+++ backend/ntt/src/main/java/com/ntt/app/AppApplication.java
@@ -0,0 +1,24 @@
+package com.ntt.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * packageName    : PACKAGE_NAME
+ * fileName       : NttApplication
+ * author         : Kim
+ * date           : 2022-10-24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-10-24        Kim       최초 생성
+ */
+@SpringBootApplication
+public class AppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+
+        System.out.println("hello world");
+    }
+}
---
+++ backend/ntt/src/main/java/com/ntt/app/translate/TranslateController.java
@@ -0,0 +1,20 @@
+package com.ntt.app.translate;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * packageName    : com.ntt.app.translate
+ * fileName       : TranslateController
+ * author         : Kim
+ * date           : 2022-10-24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-10-24        Kim       최초 생성
+ */
+@RestController
+public class TranslateController {
+
+    
+}

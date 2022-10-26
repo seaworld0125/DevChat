4f10b5b7c38883dfd857f52bdd2723f54f8d3a72
Tae kyoung Kim <mabrother98@gmail.com>
2022-10-26 18:00:56 +0900

    [🛠️ fix] 불필요 코드 제거

--- backend/ntt/src/main/java/com/ntt/app/AppApplication.java
+++ backend/ntt/src/main/java/com/ntt/app/AppApplication.java
@@ -18,7 +18,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppApplication {
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
-
-        System.out.println("hello world");
     }
 }
